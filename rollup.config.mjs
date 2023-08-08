1673f044e598c1b7ac9d5543ba7e1e24fcd2c9d7
enescang <developerindiee@gmail.com>
2023-08-08 11:02:40 +0300

    Rollup css support plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
-
+import postcss from "rollup-plugin-postcss";
 
 export default [
     {
@@ -25,13 +25,16 @@ export default [
             resolve(),
             commonjs(),
             typescript({tsconfig: "./tsconfig.json"}),
-
+            postcss({
+                plugins: []
+              })
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+        external: [/\.(css|less|scss)$/],
     },
 
 ];
--- src/TestComponent/index.tsx
+++ src/TestComponent/index.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import "./index.css";
 
 const Test = () => {
     return (
         <div>
-            <h3>Test</h3>
+            <h3 className='example'>Test</h3>
         </div>
     )
 }
